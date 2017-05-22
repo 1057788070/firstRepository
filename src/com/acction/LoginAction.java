7decf12ed8adbfd0f8ca8c0653b377f6d1dc5065
1753445107 <1753445107@qq.com>
2017-05-22 16:41:51 +0800

    firsthaha

---
+++ src/com/acction/LoginAction.java
@@ -0,0 +1,17 @@
+package com.acction;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport{
+	
+	public String execute(){
+		System.out.println("调用成功");
+		return "success";
+	}
+	
+	public String login(){
+		
+		return "success";
+	}
+
+}
---
+++ src/com/acction/UserAction.java
@@ -0,0 +1,11 @@
+package com.acction;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserAction extends ActionSupport {
+
+	public String regist(){
+		
+		return "success";
+	}
+}
